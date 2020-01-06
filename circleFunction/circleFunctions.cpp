bb1794e46171db22cd78bdacbfe8970d2635e4fb
ETTC <ETTC@107-17>
2020-01-06 10:44:17 -0500

    eee

---
+++ circleFunction/circleFunctions.cpp
@@ -0,0 +1,13 @@
+#include "circleFunctions.h"
+#include <string>
+using namespace std;
+
+double getArea(double&) {
+
+}
+double getVolume(double&) {
+
+}
+double getDifference(double&) {
+
+}
---
+++ circleFunction/circleFunctions.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+double getArea(double&);
+double getVolume(double&);
+double getDifference(double&);
